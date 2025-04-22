c5717438d03a8beb9b28fc0edf0aa3102c722b22
thelastofus3 <alekseenkomaxim456@gmail.com>
2025-04-22 20:38:17 +0200

    Update CORS Policy

--- video-frontend/src/auth/service/AuthService.jsx
+++ video-frontend/src/auth/service/AuthService.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_BACKEND_URL;
-
 export const authService = {
-    login: (data) => axios.post(`${API_URL}/api/v1/auth/login`, data),
-    register: (data) => axios.post(`${API_URL}/api/v1/auth/register`, data),
+    login: (data) => axios.post('http://localhost:8080/api/v1/auth/login', data),
+    register: (data) => axios.post('http://localhost:8080/api/v1/auth/register', data),
 };
--- video-frontend/vite.config.js
+++ video-frontend/vite.config.js
@@ -1,7 +1,6 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
@@ -15,9 +14,6 @@ export default defineConfig({
                 });
             },
         },
-    ],
-    define: {
-        'process.env': process.env
-    }
+    ]
 })
 
